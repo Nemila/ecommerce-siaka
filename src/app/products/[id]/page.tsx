bdddbcc3a200c4484e8ecf04347be0a2a321dd75
nemila <pridila.2006@gmail.com>
2024-03-26 22:58:28 +0200

    product detail page responsive

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -12,15 +12,15 @@ type Props = {
 
 const ProductDetailPage = ({ params }: Props) => {
   return (
-    <main className="flex-1 p-8">
-      <div className="container space-y-8">
-        <div className="flex gap-8">
+    <main className="flex-1">
+      <div className="container space-y-8 p-4 md:p-8">
+        <div className="flex flex-col gap-8 md:flex-row">
           <ImageMagnifier
-            className="aspect-square w-1/3 overflow-hidden rounded-md"
+            className="aspect-square w-full overflow-hidden rounded-md md:w-1/3"
             imageUrl="https://images.unsplash.com/photo-1532332248682-206cc786359f?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
 
-          <div className="w-2/3 space-y-6">
+          <div className="w-full space-y-6 md:w-2/3">
             <div className="space-y-4">
               <h2 className="text-4xl font-bold">Nom du produit {params.id}</h2>
 
@@ -47,7 +47,7 @@ const ProductDetailPage = ({ params }: Props) => {
 
         <div className="space-y-2">
           <h3 className="text-2xl font-bold">Autres Produits</h3>
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
             <ProductCard />
             <ProductCard />
             <ProductCard />
